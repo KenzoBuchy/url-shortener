dd0840a23d4db4d21300c6eb8bf1bc81c753b502
E21B719R <kenzo.buchy--petard@univ-nantes.fr>
2025-03-05 15:04:50 +0100

    projet final

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# URL Shortener - Local Development
+# Generation de meme - Local Development
 
 ## Prerequisites
 - Node.js installed
@@ -24,3 +24,55 @@ The API will be available at `http://localhost:3000` by default.
 - Any changes to your Lambda functions will trigger automatic reload
 - Check the console for endpoint URLs and port information
 - Use `Ctrl+C` to stop the local server
+
+
+## Routes API
+
+### `uploadImage`
+- **Méthode** : `POST`
+- **Content-Type** : `multipart/form-data`
+- **Paramètres** :
+  - `file` : Fichier à télécharger (format `jpg` ou `png`).
+  - `text_up` : Texte ajouter en haut de l'image. Optionnel si `text_bottom` est remplie
+  - `text_bottom` : Texte ajouter en bas de l'image. Optionnel si `text_up` est remplie
+- **Description** : Cette route permet de télécharger une image (au format `jpg` ou `png`) avec un texte. Le texte est ajouté à l'image avant qu'elle ne soit stockée dans MinIO (ou S3). L'image est ensuite rendue publique.
+
+- **Réponse** : 
+  - En cas de succès, la réponse renverra l'URL de l'image téléchargée.
+  - Exemple de réponse :
+    ```json
+    {
+      "message": "File uploaded successfully",
+      "fileUrl": "http://minio-url:9000/my-bucket/uploads/uuid-image1.jpg"
+    }
+    ```
+    - Si `text_up` et `text_bottom` ne sont pas envoyés, la réponse renverra un message d'erreur.
+  - Exemple de réponse :
+    ```json
+    {
+      "message": "Merci de remplir au moins un des deux champs" 
+    }
+    ```
+
+---
+
+### `listMemes`
+- **Méthode** : `GET`
+- **Description** : Cette route retourne une liste des images téléchargées, avec leurs URLs et dates de création.
+- **Réponse** : 
+  - Un tableau contenant des objets avec les champs `file_url` et `createdAt`.
+  - Exemple de réponse :
+    ```json
+    {
+      "imageUrls": [
+        {
+          "file_url": "http://minio-url:9000/my-bucket/uploads/uuid-image1.jpg",
+          "createdAt": "2025-03-05T10:30:00Z"
+        },
+        {
+          "file_url": "http://minio-url:9000/my-bucket/uploads/uuid-image2.jpg",
+          "createdAt": "2025-03-04T09:20:00Z"
+        }
+      ]
+    }
+    ```
--- handler.js
+++ handler.js
@@ -1,5 +1,14 @@
-const AWS = require("aws-sdk");
-const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
+
+const app = express();
+
+// Configurer Multer pour le stockage en mémoire
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient(
   process.env.IS_OFFLINE && {
@@ -10,87 +19,134 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient(
 
 const TABLE_NAME = process.env.DYNAMODB_TABLE;
 
-exports.index = async (event) => {
-  return {
-    statusCode: 404,
-    body: JSON.stringify({
-      message: "Url Shortener v1.0",
-    }),
-  };
-};
+const s3 = new AWS.S3({
+  endpoint: `http://${process.env.MINIO_ENDPOINT}:${process.env.MINIO_PORT}`,
+  accessKeyId: process.env.MINIO_ACCESS_KEY,
+  secretAccessKey: process.env.MINIO_SECRET_KEY,
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4',
+});
 
-exports.createUrl = async (event) => {
-  const { url } = JSON.parse(event.body);
-  const key = uuidv4().slice(0, 8);
+const BUCKET_NAME = process.env.MINIO_BUCKET;
 
-  const params = {
-    TableName: TABLE_NAME,
-    Item: {
-      key,
-      url,
-      createdAt: new Date().toISOString(),
-      clicks: 0,
-    },
-  };
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    // Vérifier que le fichier existe
+    if (!req.file) {
+      return res.status(400).json({ message: 'No file uploaded' });
+    }
 
-  await dynamoDb.put(params).promise();
+    if (!req.body.text_up && !req.body.text_bottom) {
+      return res.status(400).json({ message: 'Merci de remplir au moins un des deux champs' });
+    }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ key, shortUrl: `http://localhost:3000/url/${key}` }),
-  };
-};
+    const text_up = req.body.text_up ? req.body.text_up : "";
+    const text_bottom = req.body.text_bottom ? req.body.text_bottom : "";
+  
 
-exports.listUrls = async (event) => {
-  const params = {
-    TableName: TABLE_NAME,
-  };
+    const { width, height } = await sharp(req.file.buffer).metadata();
+    
+    // Créer l'élément SVG avec les dimensions de l'image
+    const svgText = `
+      <svg width="${width}" height="${height}">
+    <!-- Texte en haut au milieu -->
+    <text x="${width / 2}" y="50" font-size="30" fill="white" text-anchor="middle">${text_up}</text>
+    
+    <!-- Texte en bas au milieu -->
+    <text x="${width / 2}" y="${height - 50}" font-size="30" fill="white" text-anchor="middle">${text_bottom}</text>
+  </svg>
+    `;
 
-  const { Items } = await dynamoDb.scan(params).promise();
+    // Ajouter du texte sur l'image avec Sharp
+    const imageBuffer = await sharp(req.file.buffer)
+      .composite([{
+        input: Buffer.from(svgText),
+        gravity: 'northwest', // Position du texte (en haut à gauche)
+      }])
+      .toBuffer();
+    // Générez une clé unique pour l'image
+    const file_key = `uploads/${uuidv4()}-${req.file.originalname}`;
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(Items),
-  };
-};
-
-exports.getUrl = async (event) => {
-  const { key } = event.pathParameters;
-
-  const params = {
-    TableName: TABLE_NAME,
-    Key: {
-      key,
-    },
-  };
-
-  const { Item } = await dynamoDb.get(params).promise();
-
-  // Increase the number of clicks
-  await dynamoDb
-    .update({
-      TableName: TABLE_NAME,
-      Key: {
-        key,
-      },
-      UpdateExpression: "SET clicks = clicks + :inc",
-      ExpressionAttributeValues: {
-        ":inc": 1,
-      },
+    // Uploader l'image sur MinIO (ou S3)
+    await s3.putObject({
+      Bucket: BUCKET_NAME,
+      Key: file_key,
+      Body: imageBuffer,
+      ContentType: req.file.mimetype,
+      ACL: 'public-read',
     })
-    .promise();
+      .promise()
+      .then(() => {
+        console.log('File uploaded successfully');
+      })
+      .catch((error) => {
+        console.error('Error uploading file:', error);
+        throw error; // Renvoyer l'erreur pour la gestion du bloc catch
+      });
 
-  if (!Item) {
+      file_url = `http://${process.env.MINIO_ENDPOINT}:${process.env.MINIO_PORT}/${BUCKET_NAME}/${file_key}`;
+      const key = uuidv4().slice(0, 8);
+
+      const params = {
+        TableName: TABLE_NAME,
+        Item: {
+          key,
+          "file_key": file_key,
+          "file_url": file_url,
+          createdAt: new Date().toISOString(),
+          clicks: 0,
+        },
+      };
+    
+      await dynamoDb.put(params).promise();
+
+    // Réponse avec l'URL du fichier
+    res.status(200).json({
+      message: 'File uploaded successfully',
+      fileUrl: file_url,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'An error occurred while uploading the file',
+      error: error.message,
+    });
+  }
+});
+
+exports.listMemes = async (event) => {
+  const params = {
+    TableName: TABLE_NAME,
+  };
+
+  try {
+    const { Items } = await dynamoDb.scan(params).promise();
+
+    // Mappe les éléments pour retourner les URLs et les dates de création
+    const result = Items.map(item => ({
+      file_url: item.file_url,
+      createdAt: item.createdAt,
+    }));
+
+    // Trier les résultats par date (plus récente en premier)
+    result.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
     return {
-      statusCode: 404,
-      body: JSON.stringify({ message: "URL not found" }),
+      statusCode: 200,
+      body: JSON.stringify({ imageUrls: result }),
+    };
+  } catch (error) {
+    console.error('Error fetching memes:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'An error occurred while retrieving the images', error: error.message }),
     };
   }
-
-  return {
-    statusCode: 301,
-    headers: {
-      Location: Item.url,
-    },
-  };
 };
+
+
+
+// Utiliser Lambda pour gérer cette API Express
+const serverless = require('serverless-http');
+module.exports.uploadImage = serverless(app);
+
